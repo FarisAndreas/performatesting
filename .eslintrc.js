0499a103933ed3017e01d1e7385cf4638acb9165
faris andreas <faris.andreas@gmail.com>
2025-11-06 09:16:33 +0700

    downgrade eslin ke 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,20 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-
-// Initialize with the necessary configurations
-const compat = new FlatCompat({
-  recommendedConfig: js.configs.recommended,
-  eslintrc: true
-});
-
 module.exports = {
   env: {
     node: true,
     es2021: true,
-    jest: true, // ✅ untuk mendukung test, expect, beforeEach
+    jest: true, // ✅ mendukung test, expect, beforeEach
   },
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'script',
   },
   globals: {
-    __ENV: 'readonly',
+    __ENV: 'readonly', // ✅ untuk k6
   },
   rules: {
     'no-undef': 'error',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+    'eqeqeq': ['error', 'always'],
   },
 };
-  js.configs.recommended,
-  ...compat.config({
-    env: { 
-      es2021: true, 
-      node: true, 
-      jest: true 
-    },
-    rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-      "no-console": "off",
-      "eqeqeq": ["error", "always"]
-    }
-  })
-];
