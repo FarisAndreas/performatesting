0784740049e66548b1782539576a14f6399b219e
faris andreas <faris.andreas@gmail.com>
2025-11-07 23:41:43 +0700

    mengurangi Vus

--- loadtest/script.js
+++ loadtest/script.js
@@ -4,9 +4,9 @@ import { sleep, check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m', target: 50 },
-    { duration: '30s', target: 0 },
+    { duration: '10s', target: 20 },
+    { duration: '30s', target: 30 },
+    { duration: '10s', target: 0 },
   ],
   thresholds: {
     http_req_duration: ['p(95)<400'],
